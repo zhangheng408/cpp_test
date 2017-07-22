b716c64c43e62cc783605477b024fcbc3f7ad0cb
Zhang Heng <zhangheng@mprc.pku.edu.cn>
2017-07-23 00:18:55 +0800

    see optimizer options
    Signed-off-by: Zhang Heng <zhangheng@mprc.pku.edu.cn>

--- inline.c
+++ inline.c
@@ -1,4 +1,8 @@
 //__attribute__((no_instrument_function)) static inline void f(){
+//Alternatively you can discover which binary optimizations are enabled by -O3 by using:
+//gcc -c -Q -O3 --help=optimizers > /tmp/O3-opts
+//gcc -c -Q -O2 --help=optimizers > /tmp/O2-opts
+//diff /tmp/O2-opts /tmp/O3-opts | grep enabled
 #include<stdio.h>
 static inline void f(){
     printf("f");
